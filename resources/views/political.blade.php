2a59a3487a7467a5bdf8b9e38033df3ee9ed38b0
femidamilola <femi.adebayo1998@gmail.com>
2022-04-03 17:51:50 +0100

    Request

--- resources/views/political.blade.php
+++ resources/views/political.blade.php
@@ -20,25 +20,25 @@
 <section class="section section-lg bg-gray-100">
   <div class="container">
     <h3 class="text-center">Olubanke Oniru Leadership History</h3>
-    <div class="row row-50 justify-content-center justify-content-lg-start">
+    <div class="row row-50 justify-content-center justify-content-lg-start" style="align-items: center;">
       <div class="col-md-10 col-lg-7 wow-outer">
         <!-- Post Block-->
         <article class="post-block bg-gray-700">
-            <img class="post-block-image" src="images/leadership-cv1.jpg" alt="" width="900" height="800">
-          <div class="post-block-caption">
+            <img class="post-block-imae" src="images/leadership-cv1.jpg" alt="" width="900" height="800">
+          <!-- <div class="post-block-caption">
             <ul class="post-block-meta">
               <li class="wow-outer">
-                    <span class="wow slideInLeft">
+                    <!-- <span class="wow slideInLeft">
                         An organization to fight against police brutality and ensure that citizens of Nigeria get the justice they truly deserves
-                    </span>
+                    </span> -->
                 </li>
               <!-- <li class="wow-outer"><a class="button-winona wow slideInLeft" href="#" style="visibility: hidden; animation-name: none;">News</a></li>
               <li class="wow-outer">
                 <time class="wow slideInLeft" datetime="2018" style="visibility: hidden; animation-name: none;">April 2, 2018</time>
               </li> -->
             </ul>
-            <h4 class="post-block-title">Founder and president Competent Nigerians.</h4>
-          </div>
+            <!-- <h4 class="post-block-title">Founder and president Competent Nigerians.</h4> --
+          </div> -->
           <div class="post-block-dummy"></div>
         </article>
       </div>
@@ -71,6 +71,16 @@
             <time class="post-light-time" datetime="2018"><span class="post-light-time-big">3</span><span class="post-light-time-small"></span></time>
             <div class="post-light-main">
               <h4 class="post-light-title">Advocate for climate change prevention.</h4>
+              <ul class="post-light-meta">
+                <li>Olubanke Oniru</li>
+                <li>History</li>
+              </ul>
+            </div>
+          </article>
+          <article class="post-light wow slideInLeft">
+            <time class="post-light-time" datetime="2018"><span class="post-light-time-big">4</span><span class="post-light-time-small"></span></time>
+            <div class="post-light-main">
+              <h4 class="post-light-title">Founder and president Competent Nigerians.</h4>
               <ul class="post-light-meta">
                 <li>Olubanke Oniru</li>
                 <li>History</li>
